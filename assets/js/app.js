ac097363edb199bad100552507ce30e0650d5aa1
Aretha Walls <admin@arethawalls.com>
2018-11-20 14:08:27 -0700

    More images, more style, working on app

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    const Talk = {
+        id : 0,
+        text : '', 
+        emote : '',
+        wait : 2,
+        responses : [],
+        say() {
+            console.log(this.text);
+        }
+    }
+
+    
+
+});
